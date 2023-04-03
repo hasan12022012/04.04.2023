28164352e073c62ef98c6272e9cbd0b4e1f0a3e9
Hasan Huseynli <hasansh@code.edu.az>
2023-04-04 01:08:20 +0400

    taskAdded

---
+++ assets/script.js
@@ -0,0 +1,77 @@
+
+class Company {
+    constructor(companyN, fName) {
+        this.companyN = companyN,
+            this.fName = fName
+    }
+
+
+    employees = [
+        [],
+        []
+    ]
+
+
+
+    getData() {
+        let data = this.employees;
+        let inp1 = document.getElementById('inp1');
+        let inp2 = document.getElementById('inp2');
+
+        if (inp1.value == '' && inp2.value == '') {
+            alert('bosluqlari doldurun')
+        }
+        else if (inp1.value == '') {
+            alert('birinci bosluqu doldurun')
+        }
+        else if (inp2.value == '') {
+            alert('ikinci  bosluqu doldurun')
+        }
+
+        else {
+            data[0].push(inp1.value);
+            data[1].push(inp2.value)
+            inp1.value = '';
+            inp2.value = '';
+        }
+        
+
+    }
+
+    employeeTable() {
+        let data = this.employees;
+        let tr = `<tr>
+        <td> Name </td>
+        <td> Position </td>
+        </tr>`
+        for (let i = 0; i < data[0].length; i++) {
+            tr += `<tr>
+            <td> ${data[0][i]} </td>
+            <td> ${data[1][i]} </td>
+            </tr>` // tr = tr+
+        }
+
+        return tr;
+    }
+
+}
+
+
+const a = new Company('Code Academy', '28 may');
+const b = new Company('Code Academy', 'Nizami');
+
+let companyName = document.getElementById('companyName');
+let flialName = document.getElementById('companyF');
+let btn = document.getElementById('btn');
+let form = document.getElementById('form');
+let tbl = document.getElementById('tbl');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    a.getData();
+    tbl.innerHTML = a.employeeTable();
+})
+
+
+companyName.innerHTML = a.companyN;
+flialName.innerHTML = a.fName;
